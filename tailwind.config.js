60dcec7fa4fce8b02710354bfc53f6cdf8c2387e
Hossam Gamal Ali <hossamg122@gmail.com>
2024-07-08 04:50:42 +0300

    Feature/add tailwind color palette (#5)
    * feat: define app color palette
    
    * feat: update app title

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,39 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    colors: {
+      primary: {
+        DEFAULT: '#802BD6',
+        900: '#2e2762'
+      },
+      secondary: {
+        DEFAULT: '#7462F4'
+      },
+      success: {
+        DEFAULT: '#12b669'
+      },
+      error: {
+        DEFAULT: '#f04437'
+      },
+      neutrals: {
+        100: '#efeff1',
+        200: '#e0dfe2',
+        300: '#c0bfc5',
+        400: '#a1a0a9',
+        500: '#81808c',
+        600: '#62606f',
+        700: '#4e4d59',
+        800: '#3b3a43',
+        900: '#27262c',
+        1000: '#141316'
+      },
+      white: {
+        DEFAULT: '#ffffff'
+      },
+      black: {
+        DEFAULT: '#000000'
+      }
+    }
   },
   plugins: []
 }
