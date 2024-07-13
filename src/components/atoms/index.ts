a61675e664952ba2929ffc43e5571d8a09018348
Hossam Gamal Ali <hossamg122@gmail.com>
2024-07-14 01:24:37 +0300

    feat: implement input component (#12)

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,4 @@
 export { default as ZLoader } from './ZLoader.vue'
 export { default as ZIcon } from './ZIcon.vue'
 export { default as ZBtn } from './ZBtn.vue'
+export { default as ZInput } from './ZInput.vue'
