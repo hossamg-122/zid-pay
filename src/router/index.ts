00c399855881f368db365f97e0905c855c0f944a
hossamg122 <hossamg122@gmail.com>
2024-07-16 00:28:09 +0300

    feat: protected routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/account-details',
+      name: 'account',
+      component: HomeView,
+      meta: {
+        authRequired: true
+      }
     },
     {
       path: '/login',
-      name: 'LogIn',
+      name: 'logIn',
       component: () => import('@/views/auth/LogIn.vue')
+    },
+    {
+      path: '/sign-up',
+      name: 'signUp',
+      component: () => import('@/views/auth/SignUp.vue')
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'unknown',
+      redirect: '/404'
     },
     {
       path: '/404',
@@ -21,5 +35,18 @@ const router = createRouter({
     }
   ]
 })
+router.beforeEach((to, from, next) => {
+  const authUserStore = useAuthStore()
 
+  if (to.meta.authRequired && !authUserStore.isLoggedIn) {
+    next('/login')
+  } else {
+    next()
+  }
+})
+router.afterEach((to, from) => {
+  if (from.name && to.name !== from.name) {
+    window.scrollTo(0, 0)
+  }
+})
 export default router
