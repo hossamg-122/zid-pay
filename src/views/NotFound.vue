6c4fb5e49aaebeb3cbefcb8553858346ac444fda
hossamg122 <hossamg122@gmail.com>
2024-07-15 03:14:07 +0300

    feat: implement 404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="min-h-screen flex justify-center items-center">
+    <h1 class="text-5xl font-bold">404!</h1>
+  </div>
+</template>
+<script lang="ts">
+export default {}
+</script>
