66433c2146ce0d14d4acfe4fbfc80e53fa468b2e
hossamg122 <hossamg122@gmail.com>
2024-07-16 12:40:03 +0300

    fix: home route redirect

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      redirect: '/account'
+    },
+    {
       path: '/account',
       name: 'account',
       component: () => import('@/views/AccountView.vue'),
